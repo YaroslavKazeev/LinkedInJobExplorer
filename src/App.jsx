77bc8310599fa9bb9a13ec11149f313526ec200d
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-10-13 10:16:54 +0200

    listing and details removed to their own components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Home from "./Home.jsx";
@@ -5,12 +6,24 @@ import JobListing from "./jobListing.jsx";
 import JobDetails from "./jobDetails.jsx";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("home");
+  const pages = [
+    { id: "home", name: "Home Page", route: "/" },
+    { id: "listings", name: "Job Listings", route: "/jobListing" },
+    { id: "detail", name: "Job Detail", route: "/jobDetails" },
+  ];
+  const pageControls = {}
+    currentPage,
+    setCurrentPage,
+    pages,
+  };
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/jobListing" element={<JobListing />} />
-        <Route path="/jobDetails" element={<JobDetails />} />
+        <Route path="/" element={<Home pageControls={pageControls} />} />
+        <Route path="/jobListing" element={<JobListing pageControls={pageControls} />} />
+        <Route path="/jobDetails" element={<JobDetails pageControls={pageControls} />} />
       </Routes>
     </BrowserRouter>
   );
